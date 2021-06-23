fc6356a66bf3e10389ec7069cf4f39973df0ef76
Gunashree S <gunashrees341@gmail.com>
2021-06-23 11:32:33 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -13,7 +13,7 @@ class paper
 		this.x=x;
 		this.y=y;
 		this.r=r
-		this.image=loadImage("paper.png");
+		
 		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
 		World.add(world, this.body);
 
@@ -28,8 +28,8 @@ class paper
 			rectMode(CENTER)
 			//strokeWeight(4);
 			fill(255,0,255)
-			imageMode(CENTER);
-			image(this.image, 0,0,this.r, this.r)
+			ellipseMode(CENTER);
+			ellipse( 0,0,this.r, this.r)
 			//ellipse(0,0,this.r, this.r);
 			pop()
 			
